16e727b8a049f082d110a8985391b41425c142d5
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-02-14 21:37:23 -0300

    O método destructing

---
+++ 05 - Arrays/destructiring.js
@@ -0,0 +1,8 @@
+//Destructiring - Pega os valores de um array e armazena em variáveis
+
+const arr = [1,2,3,4]
+
+//Para cada letra, associa o valor de cada item do arrai
+let [a, b, c, d] = arr
+
+console.log(`Valor de a: ${a}, valor de b: ${b}, valor de c: ${c} e valor de d: ${d} `)
