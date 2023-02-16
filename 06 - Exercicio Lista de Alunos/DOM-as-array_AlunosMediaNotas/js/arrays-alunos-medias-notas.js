b4af4d3553ae4844061cb72f2a7637c687ac74c3
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-02-15 21:31:40 -0300

    obter o índice de nossa coleção de objetos do DOM

--- 06 - Exercicio Lista de Alunos/DOM-as-array_AlunosMediaNotas/js/arrays-alunos-medias-notas.js
+++ 06 - Exercicio Lista de Alunos/DOM-as-array_AlunosMediaNotas/js/arrays-alunos-medias-notas.js
@@ -1,5 +1,26 @@
 (function(){
 
+    //Método feito pelo chatGPT, pois o original não funcionou
+    function getIndexByAlunoNota(value, prop) {
+        const ths = document.querySelectorAll(`th[${prop}]`);
+        for (let i = 0; i < ths.length; i++) {
+          if (ths[i].getAttribute(prop) === value.toString()) {
+            return i;            
+          }
+        }
+        return -1; // caso o valor não seja encontrado
+      }   
+   
+    const INDICES_NOTAS = {
+        n1: getIndexByAlunoNota('n1','aluno-nota'),
+        n2: getIndexByAlunoNota('n2','aluno-nota'),
+        n3: getIndexByAlunoNota('n3','aluno-nota'),
+        n4:getIndexByAlunoNota('n4','aluno-nota'),
+        media:getIndexByAlunoNota('media','aluno-nota'),
+    }
+
+    console.log(INDICES_NOTAS)
+
     const trs = document.querySelectorAll("tbody tr")
 
     let x = 0
@@ -11,14 +32,14 @@
         const tds = trs[x].querySelectorAll("td")
         console.log(tds)
         media = average(
-            parseFloat(tds[1].textContent),
-            parseFloat(tds[2].textContent),
-            parseFloat(tds[3].textContent),
-            parseFloat(tds[4].textContent)
+            parseFloat(tds[INDICES_NOTAS.n1].textContent),
+            parseFloat(tds[INDICES_NOTAS.n2].textContent),
+            parseFloat(tds[INDICES_NOTAS.n3].textContent),
+            parseFloat(tds[INDICES_NOTAS.n4].textContent)
         )
         x++
 
-        tds[5].textContent = media.toFixed(2)
+        tds[INDICES_NOTAS.media].textContent = media.toFixed(2)
     }
 
 })()
