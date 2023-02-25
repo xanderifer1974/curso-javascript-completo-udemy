e45c1865c74b26a73a085b2bc02de91f118bc5f4
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-02-24 22:21:06 -0300

    Melhoria no código, utilizando o addEventListener

--- 07 - DOM_2/Desafio/TodoList/todo.js
+++ 07 - DOM_2/Desafio/TodoList/todo.js
@@ -35,4 +35,37 @@ function AdicionarTarefa(tarefa,indice){
     LITarefas[indice].remove()
 }
 
+const btnTarefa1 = document.getElementById("tarefa1")
+const btnTarefa2 = document.getElementById("tarefa2")
+const btnTarefa3 = document.getElementById("tarefa3")
+const btnTarefa4 = document.getElementById("tarefa4")
+
+console.log(btnTarefa1)
+
+btnTarefa1.addEventListener("click", function(){
+    var param2 = listaTarefas[0];
+    var param3 = 0;
+    AdicionarTarefa(param2, param3)
+});
+
+btnTarefa2.addEventListener("click", function(){
+    var param2 = listaTarefas[1];
+    var param3 = 1;
+    AdicionarTarefa(param2, param3)
+});
+
+btnTarefa3.addEventListener("click", function(){
+    var param2 = listaTarefas[2];
+    var param3 = 2;
+    AdicionarTarefa(param2, param3)
+});
+
+btnTarefa4.addEventListener("click", function(){
+    var param2 = listaTarefas[3];
+    var param3 = 3;
+    AdicionarTarefa(param2, param3)
+});
+
+
+
 
