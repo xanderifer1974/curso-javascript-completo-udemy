14c2193f9b0ccd82a4c4df99a37a9fbc193c3efa
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-03-06 21:36:50 -0300

    Adicionar Li

---
+++ 09 - TODO LIST/js/todolist.js
@@ -0,0 +1,19 @@
+; (function () { //Por questões de boas práticas, começamos com ponto e vírgula nas funções auto invocáveis
+    "use strict"
+
+    //Armazenar o DOM em variáveis
+    const itemInput = document.getElementById("item-input")
+    const todoAddForm = document.getElementById("todo-add")
+    const ul = document.getElementById("todo-list")
+
+    todoAddForm.addEventListener("submit", function (event) {
+        event.preventDefault()
+        ul.innerHTML += `<li class="todo-item">       
+        <p class="task-name">${itemInput.value}</p>     
+         </li>`
+
+    })
+
+
+
+})()
