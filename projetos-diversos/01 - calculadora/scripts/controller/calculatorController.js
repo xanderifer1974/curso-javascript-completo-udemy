74d5f5fbea5cfa06823fafd6aeb69a53fe3bd254
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-03-08 20:23:47 -0300

    método para executar operacao - parte 1

--- projetos-diversos/01 - calculadora/scripts/controller/calculatorController.js
+++ projetos-diversos/01 - calculadora/scripts/controller/calculatorController.js
@@ -2,6 +2,7 @@ class CalculatorController {
 
     constructor() {
 
+        this._operation = []
         this._locale = 'pt-BR'
         this._displayCalEl = document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
@@ -20,27 +21,99 @@ class CalculatorController {
 
     }
 
-    addEventListenerAll(element,events, fn){
+    addEventListenerAll(element, events, fn) {
 
-        events.split(' ').forEach(event =>{
+        events.split(' ').forEach(event => {
 
-            element.addEventListener(event,fn,false)
+            element.addEventListener(event, fn, false)
 
         })
 
     }
 
-    initButtonsEvents(){
+    clearAll() {
+        this._operation = []
+
+    }
+
+    clearEntry() {
+        this._operation.pop()
+
+    }
+
+    addOperation(value) {
+        this._operation.push(value)
+        console.log(this._operation)
+    }
+
+    setError() {
+        this.displayCalc = "Error"
+    }
+
+    execBtn(value) {
+
+        switch (value) {
+            case 'ac':
+                this.clearAll()
+                break;
+            case 'ce':
+                this.clearEntry()
+                break;
+            case 'soma':
+
+                break;
+            case 'subtracao':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+
+            case 'porcento':
+
+                break;
+
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+                break;
+
+
+            default:
+                this.setError()
+                break;
+        }
+    }
+
+    initButtonsEvents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
-        buttons.forEach((btn, index) =>{
-            this.addEventListenerAll(btn,"click drag", e =>{
-                console.log(btn.className.baseVal.replace("btn-",""))
-                
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, "click drag", e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-", "")
+                this.execBtn(textBtn)
+
+
             })
 
-            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
                 btn.style.cursor = "pointer"
             })
         })
@@ -48,7 +121,7 @@ class CalculatorController {
 
     setDisplayDateTime() {
 
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
             year: "numeric"
